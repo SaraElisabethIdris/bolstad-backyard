be474d462a85db3c26e361b406de3dac4dd58af9
Rie Nyhus <rie.nyhus@sparebank1.no>
2024-11-28 16:20:24 +0100

    bruker useForm

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,33 +1,32 @@
-// Register.tsx
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATHS } from '../constants';
 import { useAddParticipant } from '../api/create-participant.ts';
 import { Participant } from '../../types';
 import styles from '../register.module.less';
+import {FormProvider, useForm} from "react-hook-form";
+import {DevTool} from "@hookform/devtools";
 
 export default function Register() {
-    const [formData, setFormData] = useState<Participant>({
-        id: 0,
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: 0
-    });
+
     const [error, setError] = useState<string | null>(null);
     const navigate = useNavigate();
     const addParticipantMutation = useAddParticipant();
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
+    const form = useForm<Participant>({
+        defaultValues: {
+            id: 0,
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: 0
+        },
+    });
 
-
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const onSubmit = async (participant: Participant) => {
         setError(null);
         try {
-            await addParticipantMutation.mutateAsync(formData);
+            await addParticipantMutation.mutateAsync(participant);
             navigate(ROUTE_PATHS.PARTICIPANTS_LIST);
 
         } catch (error: any) {
@@ -38,31 +37,38 @@ export default function Register() {
     return (
             <div className={styles.container}>
                 <h1>Register for Bolstad Backyard Ultra</h1>
-                <form onSubmit={handleSubmit} className={styles.form}>
-                    <div className={styles.labelAndInput}>
-                        <label htmlFor="firstName">First Name</label>
-                        <input className={styles.input} type="text" id="firstName" name="firstName" required
-                               value={formData.firstName} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.labelAndInput}>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input className={styles.input} type="text" id="lastName" name="lastName" required
-                               value={formData.lastName} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.labelAndInput}>
-                        <label htmlFor="email">Email</label>
-                        <input className={styles.input} type="email" id="email" name="email" required
-                               value={formData.email} onChange={handleChange}/>
-                    </div>
-                    <div className={styles.labelAndInput}>
-                        <label htmlFor="phone">Phone</label>
-                        <input className={styles.input} type="text" id="phone" name="phone" required
-                               value={formData.phone} onChange={handleChange}/>
-                    </div>
-                    <button type="submit" disabled={addParticipantMutation.isLoading}>
-                        {addParticipantMutation.isLoading ? 'Registering...' : 'Register'}
-                    </button>
-                </form>
+                <FormProvider {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className={styles.form}>
+                        <div className={styles.labelAndInput}>
+                            <label htmlFor="firstName">First Name</label>
+                            <input className={styles.input} type="text" id="firstName"
+                                   {...form.register('firstName', {required: true})}
+                            />
+                        </div>
+                        <div className={styles.labelAndInput}>
+                            <label htmlFor="lastName">Last Name</label>
+                            <input className={styles.input} type="text" id="lastName"
+                                   {...form.register('lastName', {required: true})}
+                            />
+                        </div>
+                        <div className={styles.labelAndInput}>
+                            <label htmlFor="email">Email</label>
+                            <input className={styles.input} type="email" id="email"
+                                   {...form.register('email', {required: true})}
+                            />
+                        </div>
+                        <div className={styles.labelAndInput}>
+                            <label htmlFor="phone">Phone</label>
+                            <input className={styles.input} type="text" id="phone"
+                                   {...form.register('phone', {required: true})}
+                            />
+                        </div>
+                        <button type="submit" disabled={addParticipantMutation.isLoading}>
+                            {addParticipantMutation.isLoading ? 'Registering...' : 'Register'}
+                        </button>
+                    </form>
+                    <DevTool control={form.control} />
+                </FormProvider>
                 {error && (
                     <div className={styles.errorPopup}>
                         <p className={styles.errorPopup}>{error}</p>
