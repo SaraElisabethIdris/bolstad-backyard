19de28abe6e0c79d3225f19f2ffeb4c0d7684f93
Sara Idris <sara.elisabeth.idris@sparebank1.no>
2025-02-27 13:37:44 +0100

    La til bilde som ikke ble med forrige commit, og en påbegynende komponent

---
+++ frontend/src/components/SectionComponent.tsx
@@ -0,0 +1,10 @@
+export const SectionComponent = () => {
+    return (
+        <div className="section">
+            <h2>Section</h2>
+            <p>
+                This is a section component. You can use it to divide your content into sections.
+            </p>
+        </div>
+    );
+}
